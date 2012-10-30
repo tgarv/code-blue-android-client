3f15e844e0a3c7e520eb724a56a8313a4ddcd4a3
Thomas Garver <tgarv@mit.edu>
2012-10-29 22:48:34 -0400

    Added activity to handle logins, and basic navigation to login page.

--- CodeBlue/src/com/kalidu/codeblue/MainActivity.java
+++ CodeBlue/src/com/kalidu/codeblue/MainActivity.java
@@ -2,7 +2,10 @@ package com.kalidu.codeblue;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ((Button) findViewById(R.id.launch_login)).setOnClickListener(
+        	new Button.OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+					MainActivity.this.startActivity(intent);
+				}
+        	}
+        );
     }
 
     @Override
